df025af04b1fccdaa0a28bf362960d98359ab144
ZINELABIDINE <112588620+Bouabidzine@users.noreply.github.com>
2023-03-06 22:47:32 +0100

    last-changes

--- src/Form.js
+++ src/Form.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Form, Button } from 'react-bootstrap';
+import React from "react";
 
-
-export const Form = () => {
-    return (
-        <div className="form-container">
-            <Form className="signup-form">
-                <Form.Group>
-                    <Form.Control className="name-input" type="text" placeholder="name" name="name"></Form.Control>
-                    <Form.Control className="email-input" type="text" placeholder="email" name="email" ></Form.Control>
-                    <Button className="submit-button" value="submit" type="submit">submit</Button>
-                </Form.Group>
-            </Form>
-        </div>
-    )
-};
-export default Form;
+export default function Form() {
+  return (
+    <div>
+      <form>
+  <div className="form-group">
+    <label className="exampleInputEmail1">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+    <small id="emailHelp" className="form-text">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label className="exampleInputPassword1">Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+  </div>
+  <div className="form-check">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1"></input>
+    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" className="btn">Submit</button>
+</form>
+    </div>
+  );
+}
